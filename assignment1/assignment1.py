96ffa187af112c00978e5393ffe4e17ca0652f85
joapfel <kraemer_johannes@gmx.de>
2018-10-28 22:00:19 +0100

    assignment 1

---
+++ assignment1/assignment1.py
@@ -0,0 +1,102 @@
+import sys
+import csv
+import string
+from nltk.tokenize import TweetTokenizer
+
+class Index:
+    def __init__(self, size, pointer2postingsList):
+        self.size = size
+        self.pointer2postingsList = pointer2postingsList
+
+class PostingNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class TwitterIR(object):
+    __slots__ = 'id2doc', 'tokenizer', 'unicodes2remove', 'indices'
+
+    def __init__(self):
+        self.id2doc = {}
+        self.tokenizer = TweetTokenizer()
+        self.unicodes2remove = [
+            #all kinds of quotes
+            u'\u2018', u'\u2019', u'\u201a', u'\u201b', u'\u201c', u'\u201d', u'\u201e', u'\u201f', u'\u2014',
+            #all kinds of hyphens
+            u'\u002d', u'\u058a', u'\u05be', u'\u1400', u'\u1806', u'\u2010', u'\u2011', u'\u2012', u'\u2013',
+            u'\u2014', u'\u2015', u'\u2e17', u'\u2e1a', u'\u2e3a', u'\u2e3b', u'\u2e40', u'\u301c', u'\u3030',
+            u'\u30a0', u'\ufe31', u'\ufe32', u'\ufe58', u'\ufe63', u'\uff0d'
+        ]
+        self.indices = {}
+
+    def initId2doc(self, path):
+        with open(path, 'r', encoding='utf-8', newline='') as f:
+            r = csv.reader(f, delimiter='\t')
+            for line in r:
+                self.id2doc[line[1]] = line[4]
+        f.close()
+
+    def clean(self, s):
+        s = s.translate(str.maketrans('', '', string.punctuation + string.digits))
+        for u2r in self.unicodes2remove:
+            s = s.replace(u2r, '')
+        s = s.lower()
+        s = self.tokenizer.tokenize(s)
+        return s
+
+    def index(self, path):
+        self.initId2doc(path)
+        tokens2id = {}
+        for id,doc in self.id2doc.items():
+            doc = self.clean(doc)
+            for t in doc:
+                if t in tokens2id.keys():
+                    tokens2id[t].add(id)
+                else:
+                    tokens2id[t] = {id}
+
+        for t,ids in tokens2id.items():
+            size = len(ids)
+            ids = sorted(ids)
+            node = PostingNode(ids[0])
+            pointer = node
+            for id in ids[1:]:
+                n = PostingNode(id)
+                node.next = n
+                node = n
+            i = Index(size, pointer)
+            self.indices[t] = i
+
+    def _query(self, term):
+        return self.indices[term]
+
+    def query(self, t1, t2):
+        intersection = []
+        pointer1 = self._query(t1).pointer2postingsList
+        pointer2 = self._query(t2).pointer2postingsList
+        while pointer1 and pointer2:
+            val1 = pointer1.val
+            val2 = pointer2.val
+            if val1 == val2:
+                intersection.append(val1)
+                pointer1 = pointer1.next
+                pointer2 = pointer2.next
+            elif val1 > val2:
+                pointer2 = pointer2.next
+            elif val1 < val2:
+                pointer1 = pointer1.next
+        return intersection
+
+
+
+
+if __name__ == '__main__':
+    twitterIR = TwitterIR()
+    twitterIR.index('tweets.csv')
+
+    index = twitterIR.query('nacht', 'schlafen')
+    print(len(index))
+
+    for id in index:
+        print(twitterIR.id2doc[id])
+
